5c348f4f98bd50d0d2c4e52643089fbbb58cd573
kellescc <kellescc@gmail.com>
2026-02-27 18:33:17 +0000

    Basic convo test

---
+++ app/pricing/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Pricing = () => {
+  return (
+    //<div className='text-3xl font-bold flex h-screen justify-center items-center'>Pricing</div>
+    //<iframe></iframe>
+    <iframe src="https://wikipedia.com/" width="1200" height="900" title="Agric Ecosystem"></iframe>
+  )
+}
+
+export default Pricing
